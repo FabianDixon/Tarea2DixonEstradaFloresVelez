e5e523308b2fbd1b1dac497802bdde1e5ac3808b
FabianDixon <49299919+FabianDixon@users.noreply.github.com>
2019-04-05 01:59:47 -0600

    Código arreglado con Flake8

---
+++ CodigoArreglado.py
@@ -0,0 +1,11 @@
+print("""Con el siguiente programa,usted obtendrá algún
+mensaje dependiendo de dos números que va a ingresar.""")
+x = int(input("Digite un número"))
+y = int(input("Digite otro número"))
+
+if y == x ** 2:
+    print("El segundo número es el cuadrado exacto del primero")
+elif y < x ** 2:
+    print("El segundo es menor que el cuadrado del primero")
+elif y > x ** 2:
+    print("El segundo es mayor que el cuadrado del primero")
